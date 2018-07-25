35a6fca570e50938a59c21c5ec2ebdaa76637a47
bghalami <benjaminghalami@gmail.com>
2018-07-24 20:43:33 -0600

    doing my best...

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -47,4 +47,60 @@ class SalesAnalyst
     (Math.sqrt(total / (item_count.length - 1))).round(2)
   end
 
+  def merchants_with_high_item_count
+    the_mark = average_items_per_merchant + average_items_per_merchant_standard_deviation
+    merchant_hash = items_by_merchant_id(@items.items)
+    split = @merchants.merchants.map do |merchant|
+      if merchant_hash[merchant.id]
+        if ((merchant_hash[merchant.id]).count - the_mark) >= 0
+          merchant
+        end
+      end
+    end
+    split.compact
+  end
+
+  def average_item_price_for_merchant(merchant_id)
+    split = @items.find_all_by_merchant_id(merchant_id)
+
+    sum_of_prices = split.inject(0) do |sum, item|
+      sum += (item.unit_price)
+    end
+
+    (sum_of_prices / split.count).round(2)
+  end
+
+  def average_average_price_per_merchant
+    merchant_hash = items_by_merchant_id(@items.items)
+    average_merchant_average = merchant_hash.map do |merchant_id, items|
+        average_item_price_for_merchant(merchant_id)
+    end
+    averages_summed = average_merchant_average.inject(0) do |sum, bigd|
+      sum += bigd
+    end
+    (averages_summed / @merchants.merchants.count).round(2)
+  end
+
+  # def price_average_standard_deviation
+  #   average_items = average_items_per_merchant
+  #   merchant_hash = items_by_merchant_id(@items.items)
+  #   item_count = item_count_by_merchant_id(merchant_hash)
+  #   abs_differences = item_count.map do |count|
+  #     ((count - average_items).abs) ** 2.0
+  #   end
+  #   total = abs_differences.inject(0) do |sum, number|
+  #     sum += number
+  #   end
+  #   (Math.sqrt(total / (item_count.length - 1))).round(2)
+  # end
+  def total_item_price
+    total = @items.items.inject(0) do |sum, item|
+      sum += item.unit_price
+    end
+  end
+
+  def golden_items
+    average_price = total_item_price / @items.items.count
+    
+  end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -13,7 +13,7 @@ class SalesAnalystTest < Minitest::Test
       :unit_price  => "1200",
       :updated_at  => Time.now,
       :created_at  => Time.now,
-      :merchant_id => "14"
+      :merchant_id => "13"
       })
 
     @item_2 = Item.new({
@@ -23,7 +23,7 @@ class SalesAnalystTest < Minitest::Test
       :unit_price  => "1400",
       :updated_at  => Time.now,
       :created_at  => Time.now,
-      :merchant_id => "14"
+      :merchant_id => "13"
       })
 
     @item_3 = Item.new({
@@ -39,8 +39,8 @@ class SalesAnalystTest < Minitest::Test
     @item_array = [@item_1, @item_2, @item_3]
 
     @merchant1 = Merchant.new({id: 13, name: "Ben"})
-    @merchant2 = Merchant.new({id: 13, name: "Jesse"})
-    @merchant3 = Merchant.new({id: 15, name: "Sal"})
+    @merchant2 = Merchant.new({id: 14, name: "Sal"})
+    @merchant3 = Merchant.new({id: 15, name: "Jesse"})
 
     @merchant_array = [@merchant1, @merchant2, @merchant3]
     @sales_engine = SalesEngine.new(@item_array, @merchant_array)
@@ -57,5 +57,25 @@ class SalesAnalystTest < Minitest::Test
 
   def test_it_can_return_items_per_merchant_standard_deviation
     assert_equal 0.71, @analyst.average_items_per_merchant_standard_deviation
+  end
+
+  def test_it_returns_merchants_above_one_standard_deviation
+    assert_equal [], @analyst.merchants_with_high_item_count
+  end
+
+  def test_it_returns_the_average_item_price_for_given_merchant
+    assert_equal BigDecimal(13.00, 4), @analyst.average_item_price_for_merchant(13)
+  end
+
+  def test_it_returns_the_average_of_the_average_price_of_merchants
+    assert_equal BigDecimal(7.00,3), @analyst.average_average_price_per_merchant
+  end
+
+  def test_it_returns_total_item_price
+    assert_equal BigDecimal(34.00,4), @analyst.total_item_price
+  end
+
+  def test_it_returns_items_2_standard_deviations_above_item_average
+    assert_equal [], @analyst.golden_items
   end
 end
